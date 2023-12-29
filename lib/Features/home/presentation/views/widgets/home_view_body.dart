59cd8494fbe417a037aa477915e087cedcac8d4b
Mazen Gamal <mazeng18@gmail.com>
2023-12-29 14:37:15 +0200

    add list view item to home body

--- lib/Features/home/presentation/views/widgets/home_view_body.dart
+++ lib/Features/home/presentation/views/widgets/home_view_body.dart
@@ -1,14 +1,15 @@
 import 'package:flutter/material.dart';
 
+import 'list_view_item.dart';
+
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-
-      ],
+    return const Column(
+      children: [BuildListViewItem()],
     );
   }
 }
+
