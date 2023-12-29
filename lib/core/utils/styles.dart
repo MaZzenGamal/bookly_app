f184bda68b753c14b3fbaf7fca1fb085520c01bd
Mazen Gamal <mazeng18@gmail.com>
2023-12-29 15:39:21 +0200

    add font family

--- lib/core/utils/styles.dart
+++ lib/core/utils/styles.dart
@@ -3,6 +3,6 @@ import 'package:flutter/cupertino.dart';
 abstract class Styles{
   static const titleNormal = TextStyle(
     fontSize: 18,
-    fontWeight:FontWeight.w500
+    fontWeight:FontWeight.w600
   );
 }
