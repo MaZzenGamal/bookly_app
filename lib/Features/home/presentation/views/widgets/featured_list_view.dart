04295e6c648080151adcdb1dfecda444cc08420d
Mazen Gamal <mazeng18@gmail.com>
2023-12-29 15:08:27 +0200

    add featured_list_view

---
+++ lib/Features/home/presentation/views/widgets/featured_list_view.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+import 'featured_list_view_item.dart';
+
+class FeaturesBooksListView extends StatelessWidget {
+  const FeaturesBooksListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 20),
+      child: SizedBox(
+        height: MediaQuery.of(context).size.height * 0.3,
+        child: ListView.separated(
+            scrollDirection: Axis.horizontal,
+            itemBuilder: (context,index){
+              return const BuildFeaturedListViewItem();
+            }, separatorBuilder:(context,index){
+          return const SizedBox(width: 16,);
+        }, itemCount: 5),
+      ),
+    );
+  }
+}
--- lib/Features/home/presentation/views/widgets/home_view_body.dart
+++ lib/Features/home/presentation/views/widgets/home_view_body.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import 'featured_list_view_item.dart';
+import 'featured_list_view.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
@@ -20,23 +20,4 @@ class HomeViewBody extends StatelessWidget {
   }
 }
 
-class FeaturesBooksListView extends StatelessWidget {
-  const FeaturesBooksListView({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(left: 20),
-      child: SizedBox(
-        height: MediaQuery.of(context).size.height * 0.3,
-        child: ListView.separated(
-          scrollDirection: Axis.horizontal,
-            itemBuilder: (context,index){
-              return const BuildFeaturedListViewItem();
-            }, separatorBuilder:(context,index){
-              return const SizedBox(width: 16,);
-        }, itemCount: 5),
-      ),
-    );
-  }
-}
