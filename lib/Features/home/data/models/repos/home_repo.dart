8164501ddfdf90a40ba3a26694c42722bffa7c68
Mazen Gamal <mazeng18@gmail.com>
2024-01-02 11:49:21 +0200

    create home ropo methode and failure file

--- lib/Features/home/data/models/repos/home_repo.dart
+++ lib/Features/home/data/models/repos/home_repo.dart
@@ -1,7 +1,9 @@
+import 'package:bookly/Features/home/data/models/book_model/Book_model.dart';
+import 'package:bookly/core/errors/failure.dart';
+import 'package:dartz/dartz.dart';
 
+abstract class HomeRepo {
+  Future<Either<Failure, List<BookModel>>> fetchBestSellerBooks();
 
-abstract class HomeRepo{
-
-  fetchBestSellerBooks();
-  fetchFeaturedBooks();
+  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks();
 }
